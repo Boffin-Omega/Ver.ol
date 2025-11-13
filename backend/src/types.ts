7e7f6c401e92fd83a7e5605f243bc286017ae260
CodeWithBhuvan <bhuvanms1605@gmail.com>
2025-11-13 20:27:27 +0530

    feat(types): introduced edit as a new type of Change

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -8,23 +8,25 @@ interface node{
     gridFSFileId: string|null;
     timestamp: Date;
 }
-export type Change =
-  | {
+export type Change = | {
       type: "rename";
       nodeId: string;
       payload: { oldName:string,newName: string };
-    }
-  | {
+    } |
+    {
       type: "move";
       nodeId: string;
       payload: { src:string, dest:string, newParentId: string };
-    }
-  | {
+    } |
+    {
       type: "delete";
       nodeId: string;
-      payload: {
-        deletedNodeName:string
-      };
-    };
+      payload: { deletedNodeName:string };
+    } |
+    {
+      type: "edit";
+      nodeId: string;
+      payload: {newContent: string};
+    }
 export type UINode = node & { isExpanded?: boolean; children?: UINode[] };
 
